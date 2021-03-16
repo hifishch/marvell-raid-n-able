51286ecbbd4f6cba747eefb2f8425cea7499b282
hifishch <56207177+hifishch@users.noreply.github.com>
2021-03-16 22:55:46 +0100

    Added comments
    Commented code for better understanding.

--- Check-Marvell-RAID-VD.ps1
+++ Check-Marvell-RAID-VD.ps1
@@ -1,7 +1,7 @@
 ﻿#Powershell Marvell RAID VD-Check for Solarwinds RMM
 #Author: Andreas Walker a.walker@glaronia.ch
 #Licence: GNU General Public License v3.0
-#Version: 1.0.0 / 16.03.2021
+#Version: 1.0.1 / 16.03.2021
 
 Param (
 [Parameter(Mandatory=$true)][int]$vdid
@@ -9,21 +9,26 @@ Param (
 
 $cli = "C:\Program Files (x86)\Marvell\storage\interface\mvsetup.exe"
 
+#Check if MatvellCLI is installed.
 if (!(Test-Path $cli))
     {
-    Write-Host 'ERROR - Missing Marvell CLI! Please Download from https://support.hpe.com/hpsc/swd/public/detail?swItemId=MTX_17d87af242a144cbb3a954352c&swEnvO'
+    Write-Host 'ERROR - Missing MarvellCLI! Please Download from https://support.hpe.com/hpsc/swd/public/detail?swItemId=MTX_17d87af242a144cbb3a954352c&swEnvO'
     exit 1001
     }
 
+#Get output from MarvellCLI and remove unwanted lines.
 $vars = & "$cli" "info -o vd".split() | select -skip 3 | select -SkipLast 2
 
+#Dirty solution to filter VD-ID. May requires ajustment if output of MarvellCLI has different length.
 $StartLine = $vdid * 13
-
 $Result = $vars | select -skip $StartLine | select -First 12
 
+#Filter Get status of VD.
 $Status = $Result | Where-Object {$_ -match "status:" } | Where-Object {$_ -notmatch "BGA status:" }
 $Status = $Status -replace "^status:\s{1,}",""
 
+#Generating messages for SolarWinds RMM
+#Comment out $Result for less verbose output.
 if ($Status -ne 'functional')
     {
     Write-Host 'ERROR - Virtual Disk' $vdid 'has status' $Status
@@ -37,6 +42,6 @@ if ($Status -ne 'functional')
         exit 1
         }
 
-
+#Catch unexpected end of script
 Write-Host 'ERROR - Script came to an unexpected end!'
 exit 1001
